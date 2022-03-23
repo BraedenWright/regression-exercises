48d2dccfe6f7324f21473e9af43bb9427fe8f42f
Braeden Wright <braeden.wright21@gmail.com>
2022-03-23 10:49:53 -0500

    main

--- evaluate.py
+++ evaluate.py
@@ -56,3 +56,65 @@ def regression_metrics(residual, baseline_residual, df):
     print(f'Baseline RMSE --> {Baseline_RMSE:.1f}')
 
 
+
+    
+    
+    
+def rfe_feature_rankings(x_scaled, x, y, k):
+    '''
+    Takes in the predictors, the target, and the number of features to select,
+    and it should return a database of the features ranked by importance
+    '''
+    
+    # Make it
+    lm = sklearn.linear_model.LinearRegression()
+    rfe = sklearn.feature_selection.RFE(lm, n_features_to_select=k)
+
+    # Fit it
+    rfe.fit(x_scaled, y)
+    
+    var_ranks = rfe.ranking_
+    var_names = x_train.columns.tolist()
+    ranks = pd.DataFrame({'Var': var_names, 'Rank': var_ranks})
+    ranks = ranks.sort_values(by="Rank", ascending=True)
+    return ranks
+
+
+
+
+def rfe(x_scaled, x, y, k):
+    '''
+    Takes in the predictors, the target, and the number of features to select,
+    and it should return the top k features based on the RFE class. 
+    '''
+    # Make it
+    lm = sklearn.linear_model.LinearRegression()
+    rfe = sklearn.feature_selection.RFE(lm, n_features_to_select=k)
+
+    # Fit it
+    rfe.fit(x_scaled, y)
+    
+    # Use it
+    features_to_use = x.columns[rfe.support_].tolist()
+    
+    #
+    #all_rankings = show_features_rankings(x, rfe)
+    
+    return features_to_use
+
+
+
+
+def select_kbest(x_scaled, x, y, k):
+    '''
+    Takes in an x and y dataframe and the number of features to select
+    and returns the names of the top features based on SelectKBest
+    '''
+    # Make it
+    kbest = sklearn.feature_selection.SelectKBest(sklearn.feature_selection.f_regression, k=k)
+
+    # Fit tit
+    kbest.fit(x_scaled, y)
+    
+    # Use it 
+    return x.columns[kbest.get_support()].tolist()
